e106800f12c159f94448b8bf9292b767c6806418
Toughie <airj907@gmail.com>
2023-02-27 22:56:30 +0900

    Feat: 네트워크 통신 모델 구현

---
+++ musicSearchApp/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  musicSearchApp
+//
+//  Created by KimChoonSik on 2023/02/27.
+//
+
+import Foundation
+//MARK: - 매직 스트링 사용을 지양하기 위한 NameSpace
+// 열거형 혹은 구조체로 선언 가능
+
+//열거형은 case 자체가 데이터이기 때문에 저장속성을 만들 수 없음 -> 타입 저장속성은 생성 가능
+public enum MusicApi {
+    static let requestUrl = "https://itunes.apple.com/search?"
+    static let mediaParam = "media=music"
+}
--- musicSearchApp/Networking/NetworkManager.swift
+++ musicSearchApp/Networking/NetworkManager.swift
@@ -6,15 +6,12 @@
 //
 
 import Foundation
-
+// 네트워크 통신 에러 처리를 위한 에러 정의
 enum NetworkError: Error {
-    case one
-    case two
-    case three
+    case networkingError
+    case dataError
+    case parseError
 }
-
-
-
 //MARK: - 네트워킹 클래스 모델
 
 final class Networkmanager {
@@ -25,14 +22,71 @@ final class Networkmanager {
     //싱글톤 패턴 구현을 위한 생성자 - let a = NetworkManager() 같은 객체 생성 방지
     private init() {}
     
+    // 콜백 함수가 길기 때문에 가독성을 위해 typealias 활용
+    typealias NetworkCompletion = (Result<[Music], NetworkError>) -> Void
+    
     //네트워킹 요청하는 함수 (음악 데이터 가져오기)
     //네트워킹의 경우 비동기처리를 하기 때문에, 리턴값이 있는 형태로 함수를 설계하면 안됨 -> 콜백함수 활용해야함
-//    func fetchMusic(searchTerm: String, completion: @escaping (Result<[Music], NetworkError>) -> Void {
-//        let url = "
-//        
-//    }
+    //네트워크 통신 성공,실패 여부 처리를 위해 Result타입 사용
+    func fetchMusic(searchTerm: String, completion: @escaping NetworkCompletion) {
+        let urlString = "\(MusicApi.requestUrl)\(MusicApi.mediaParam)&term=\(searchTerm)"
+        
+        performRequest(with: urlString) { result in
+            completion(result)
+        }
+    }
     
+    //실제로 서버에 Request하는 함수 (비동기적 실행) -> 클로저 방식으로 끝난 시점을 전달 받음
+    private func performRequest(with urlString: String, completion: @escaping NetworkCompletion) {
+        
+        // 1. Url 객체 생성
+        guard let url = URL(string: urlString) else { return }
+        // 2. Url Session 객체 생성 (브라우저의 탭바 같은 느낌으로 이해)
+        let session = URLSession(configuration: .default)
+        
+        //비동기적으로 동작하는 dataTask(일시정지 상태로 시작해서 추후 resume 해줘야함)
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            //에러 여부 먼저 체크
+            if error != nil {
+                print(error!)
+                completion(.failure(.networkingError))
+                return
+            }
+            
+            guard let safeData = data else {
+                completion(.failure(.dataError))
+                return
+            }
+            //Call to method 'parseJason' in closure requires explicit use of 'self' to make capture semantics explicit
+            //클로저 캡처리스트 참고
+            //현재 아래 코드가 클로저 안에서 실행되고 있음 (parseJason 함수가 나중에 실행됨)
+            //명시적으로 클로저가 self를 캡처해서 메모리 누수가 일어나지 않도록 다른 추가적인 값의 참조를 방지하는것.
+            if let musics = self.parseJason(safeData) {
+                print("Parse 실행")
+                completion(.success(musics))
+            } else {
+                print("Parse 실패")
+                completion(.failure(.parseError))
+            }
+        }
+        task.resume()
+    }
+    
+    //Json Parsing 하는 함수 (decoding) 서버에서 Json 형식으로 받아온 데이터를 필요한 swift 형태로 바꾸는 과정
+    private func parseJason(_ musicData: Data) -> [Music]? {
+        //decode 함수가 에러를 던질 수 있는 함수이기에 do - catch 활용해 처리
+    
+        do { // MusicData 형태로 변환해야 하니, 메타타입을 파라미터로 넣어줘야함.
+            let parsedData = try JSONDecoder().decode(MusicData.self, from: musicData)
+            return parsedData.results
+            //MusicData의 results 속성이 [Music] 형태임
+        }
+        catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
 }
-
 
 
