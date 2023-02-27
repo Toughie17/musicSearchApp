982928cd03ad410482a00be899b74a5e431c9edb
Toughie <airj907@gmail.com>
2023-02-27 21:29:17 +0900

    First commit

---
+++ musicSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicSearchApp
+//
+//  Created by KimChoonSik on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
